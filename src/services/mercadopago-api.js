a6ef57117340c67b5aae0a91dd4cdd9b2409dc51
Joao <joaoplay16@gmail.com>
2021-03-02 21:29:27 -0300

    pasta /build adicionada

--- src/services/mercadopago-api.js
+++ src/services/mercadopago-api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 const mpApi = axios.create({
-  baseURL: `${window.location.hostname}:${process.env.PORT}`,
+  baseURL: `${process.env.BASE_URI}:${process.env.PORT}`,
 })
+
+console.log("URI", `${process.env.BASE_URI}:${process.env.PORT}`)
 
 export const payNow =  (data) => {
  return mpApi.post('/process_payment', data)
