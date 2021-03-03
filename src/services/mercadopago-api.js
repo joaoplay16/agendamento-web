3da93ec7a9f5a9c4d17d62f5ecc7ebeb6b55a984
Joao <joaoplay16@gmail.com>
2021-03-02 21:50:09 -0300

    pasta /build adicionada

--- src/services/mercadopago-api.js
+++ src/services/mercadopago-api.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
 const mpApi = axios.create({
-  baseURL: `https://raissequeiroz.herokuapp.com:${process.env.PORT}`,
+  baseURL: `https://raissequeiroz.herokuapp.com`,
 })
 
 
 export const payNow =  (data) => {
-console.log("URI", `${process.env.BASE_URI}:${process.env.PORT}`)
 
  return mpApi.post('/process_payment', data)
 }
