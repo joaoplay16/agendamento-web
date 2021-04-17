3235c75f5f85609f70160640a436efaa177659c8
Joao Pedro <joaoplay16@gmail.com>
2021-04-17 14:27:53 -0300

    set environment variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
-import firebaseConfig from './firebaseConfig'
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp({
+    apiKey: "AIzaSyC7LoaM9pv0tdzjnPDFXNyXWrmqMwC7LXQ",
+    authDomain: "agendamento-web-ea083.firebaseapp.com",
+    projectId: "agendamento-web-ea083",
+    storageBucket: "agendamento-web-ea083.appspot.com",
+    messagingSenderId: "227455752712",
+    appId: "1:227455752712:web:6b03c4cf23da9c14910fbd",
+    measurementId: "G-TEKG6V9T0W"
+})
 }
 
 export default firebase
--- src/services/firebaseConfigExample.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "<your-api-key>",
-    authDomain: "<your-auth-domain>",
-    projectId: "<your-project-id>",
-    storageBucket: "<your-storage-bucket>",
-    messagingSenderId: "<your-messaging-sender-id>",
-    appId: "<your-app-id>",
-    measurementId: "<your-measurement-id>"
-}
