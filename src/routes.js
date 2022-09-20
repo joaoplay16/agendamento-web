7269165798cd9b2606e4c3f419cfc05a3ced965d
João Pedro <joaoplay16@gmail.com>
2022-09-20 10:59:16 -0300

    admin navigation routes normalized

--- src/routes.js
+++ src/routes.js
@@ -6,17 +6,16 @@ export const CHECKOUT = `checkout`
 export const CHOOSE_PROFESSIONAL = `escolher-profissional`
 export const CHOOSE_DATE = `escolher-data`
 
-export const ADMIN = '/admin'
-export const ADMIN_LOGIN = `${ADMIN}/login`
-
-export const ADMIN_PROCEDURES = `${ADMIN}/procedimentos`
-export const ADMIN_PROCEDURES_ADD = `${ADMIN}/procedimentos/novo`
-export const ADMIN_PROCEDURES_UPDATE = `${ADMIN}/procedimentos/atualizar`
-export const ADMIN_PROFESSIONALS = `${ADMIN}/profissionais`
-export const ADMIN_PROFESSIONALS_ADD = `${ADMIN}/profissionais/novo`
-export const ADMIN_PROFESSIONALS_UPDATE = `${ADMIN}/profissionais/atualizar`
-export const ADMIN_SCHEDULES = `${ADMIN}/agendamentos`
-export const ADMIN_SETTINGS = `${ADMIN}/configuracoes`
+export const ADMIN = 'admin'
+export const ADMIN_LOGIN = `login`
+export const ADMIN_PROCEDURES = `procedimentos`
+export const ADMIN_PROCEDURES_ADD = `novo`
+export const ADMIN_PROCEDURES_UPDATE = `atualizar`
+export const ADMIN_PROFESSIONALS = `profissionais`
+export const ADMIN_PROFESSIONALS_ADD = `novo`
+export const ADMIN_PROFESSIONALS_UPDATE = `atualizar`
+export const ADMIN_SCHEDULES = `agendamentos`
+export const ADMIN_SETTINGS = `configuracoes`
 
 export const navigationRoutes = {
   LOGIN: `/${LOGIN}`,
@@ -26,4 +25,17 @@ export const navigationRoutes = {
   CHECKOUT: `/${SCHEDULE}/${CHECKOUT}`,
   CHOOSE_DATE: `/${SCHEDULE}/${CHOOSE_DATE}`,
   CHOOSE_PROFESSIONAL: `/${SCHEDULE}/${CHOOSE_PROFESSIONAL}`,
+}
+
+export const adminNavigationRoutes = {
+  ADMIN: `/${ADMIN}`,
+  ADMIN_LOGIN: `/${ADMIN_LOGIN}`,
+  ADMIN_PROCEDURES: `/${ADMIN}/${ADMIN_PROCEDURES}`,
+  ADMIN_PROCEDURES_ADD: `/${ADMIN}/${ADMIN_PROCEDURES}/${ADMIN_PROCEDURES_ADD}`,
+  ADMIN_PROCEDURES_UPDATE: `/${ADMIN}/${ADMIN_PROCEDURES}/${ADMIN_PROCEDURES_UPDATE}`,
+  ADMIN_PROFESSIONALS: `/${ADMIN}/${ADMIN_PROFESSIONALS}`,
+  ADMIN_PROFESSIONALS_ADD: `/${ADMIN}/${ADMIN_PROFESSIONALS}/${ADMIN_PROFESSIONALS_ADD}`,
+  ADMIN_PROCEDURES_UPDATE : `/${ADMIN}/${ADMIN_PROFESSIONALS}/${ADMIN_PROFESSIONALS_UPDATE}`,
+  ADMIN_SCHEDULES: `/${ADMIN}/${ADMIN_SCHEDULES}`,
+  ADMIN_SETTINGS: `/${ADMIN}/${ADMIN_SETTINGS}`
 }
