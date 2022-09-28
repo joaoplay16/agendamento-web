f34e85f8ab7bcf2d72a02c2df75d6b8c4f199705
João Pedro <joaoplay16@gmail.com>
2022-09-28 20:50:30 -0300

    spacing adjust

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,26 +1,25 @@
-import React, { Suspense } from 'react'
-import {
-  withStyles
-} from '@material-ui/core'
-import Header from './header'
-import { Outlet } from 'react-router-dom'
-import { Footer } from 'ui'
+import React, { Suspense } from "react"
+import { withStyles } from "@material-ui/core"
+import Header from "./header"
+import { Outlet } from "react-router-dom"
+import { Footer } from "ui"
 
 const Main = () => {
   return (
     <>
       <Header />
       <Spacer />
-      <Suspense fallback='Loading'>
-        <Outlet/>
+      <Suspense fallback="Loading">
+        <Outlet />
       </Suspense>
+      <Spacer />
       <Footer />
     </>
   )
 }
 
 const style = (theme) => ({
-  main: theme.mixins.toolbar
+  main: theme.mixins.toolbar,
 })
 
 const Spacer = withStyles(style)(({ classes }) => (
