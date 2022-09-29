ecfb72cfe62fab700deb64c67e1c98594287bdb7
João Pedro <joaoplay16@gmail.com>
2022-09-29 20:39:58 -0300

    progress removed from admin login

--- src/app-for-admin.js
+++ src/app-for-admin.js
@@ -1,39 +1,26 @@
-import React, { Suspense, lazy, useEffect, useState } from "react"
+import React, { Suspense, lazy, useEffect } from "react"
 import { Route, Routes, Navigate, useLocation } from "react-router-dom"
 import { LinearProgress } from "@material-ui/core"
 import { adminNavigationRoutes as navRoutes, ADMIN, ADMIN_LOGIN } from "routes"
 import { useAdminAuth } from "hooks"
 
 const Main = lazy(() => import("pages/admin/main"))
-const AdminLogin = React.lazy(() => import("pages/admin/login"))
+const AdminLogin = lazy(() => import("pages/admin/login"))
 
 function AppForAdmin() {
   const location = useLocation()
 
   const { adminInfo, verifyAdminIsLoggedIn } = useAdminAuth()
-  const [didCheckUserIn, setDidCheckUserIn] = useState(false)
   const isLoggedIn = adminInfo.isLoggedIn
 
   useEffect(() => {
     verifyAdminIsLoggedIn()
-
   }, [])
   
-  useEffect(() => {
-    setDidCheckUserIn(true)
-  }, [isLoggedIn])
-
   useEffect(() => {
     console.log("admin logged? ", adminInfo.isLoggedIn)
   }, [isLoggedIn])
 
-  useEffect(() => {
-    console.log("didCheckUserIn? ", didCheckUserIn)
-  }, [didCheckUserIn])
-
-  if (didCheckUserIn == false) {
-    return <LinearProgress />
-  }
 
   if (isLoggedIn && location.pathname === navRoutes.ADMIN_LOGIN) {
     return <Navigate to={navRoutes.ADMIN} />
