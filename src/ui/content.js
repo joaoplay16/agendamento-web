4f4c9e3a0427a695730ca2135ee99a0322153992
João Pedro <joaoplay16@gmail.com>
2022-10-01 19:00:01 -0300

    change height and component type

--- src/ui/content.js
+++ src/ui/content.js
@@ -1,6 +1,8 @@
+import { Grid } from '@material-ui/core'
 import styled from 'styled-components'
-const Content = styled.main`
+const Content = styled(Grid)`
   flex-grow: 1;
   padding: ${({ theme }) => theme.spacing(3)}px;
+  height: 100vh;
 `
 export default Content
