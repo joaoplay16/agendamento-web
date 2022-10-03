13cde2faabc2c821d6ee6e1922b8ba75e1ad7728
João Pedro <joaoplay16@gmail.com>
2022-10-03 19:43:56 -0300

    add background to selection pages

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,2 @@
 export { default as BricksBackground } from "./bricks.svg"
+export { default as CrackedSurface } from "./cracked-surface.png"
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -3,6 +3,8 @@ import { withStyles } from "@material-ui/core"
 import Header from "./header"
 import { Outlet } from "react-router-dom"
 import { Footer } from "ui"
+import styled from "styled-components"
+import { CrackedSurface } from "assets"
 
 const Main = () => {
   return (
@@ -10,7 +12,9 @@ const Main = () => {
       <Header />
       <Spacer />
       <Suspense fallback="Loading">
-        <Outlet />
+        <Background>
+          <Outlet />
+        </Background>
       </Suspense>
       <Spacer />
       <Footer />
@@ -25,5 +29,21 @@ const style = (theme) => ({
 const Spacer = withStyles(style)(({ classes }) => (
   <div className={classes.main} />
 ))
+
+const Background = styled.main`
+  height: 100vh;
+  position: relative;
+
+::before{
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(${CrackedSurface});
+  background-repeat: repeat;
+  opacity: 0.2;
+  z-index: -1;
+}
+`
 
 export default Main
